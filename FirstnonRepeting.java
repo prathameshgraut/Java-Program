ced708abca7224361ed2fcc68dbe1dbb0bd154c2
Prathamesh Raut. <prathamesh1687@gmail.com>
2025-09-20 10:10:11 +0530

    Add files via upload

---
+++ FirstnonRepeting.java
@@ -0,0 +1,28 @@
+package innoshriPVTLTD;
+
+public class FirstnonRepeting {
+
+	public static void main(String[] args) {
+		String str ="prathamesh";
+		char res=0;
+		for(int i=0;i<str.length();i++) {
+			char ch = str.charAt(i);
+			int count=0;
+			for(int j=0;j<str.length();j++) {
+				if(ch == str.charAt(j)) {
+					count++;
+				}
+			}
+			if(count==1) {
+				res=ch;
+//				break;
+			}
+		}
+		if(res==0) {
+			System.out.println(res);
+		}else {
+			System.out.println(res);
+		}
+	}
+
+}
